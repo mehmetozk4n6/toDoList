fe5f37f91767b55b7eca42c477ff4358c0a3ed67
Mehmet Özkan <81770287+mehmetozk4n6@users.noreply.github.com>
2022-01-16 22:17:11 +0300

    Add files via upload

---
+++ js/todolist1.js
@@ -0,0 +1,72 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add list item values to Local Storage
+
+for (i = 0; i < myNodelist.length; i++) {
+    localStorage.setItem(`eleman ${i+1}`,myNodelist[i].innerText)
+  }
+
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("task").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("task").value = ""; // clear input area
+
+  // add a close item to new element
+
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+  $("#liveToast").toast("show");
+
+  // add a close function to new item
+  
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+
+
+  // add new item to local storage
+  localStorage.setItem(`eleman ${localStorage.length+1}`,li.innerText)
+
+}
